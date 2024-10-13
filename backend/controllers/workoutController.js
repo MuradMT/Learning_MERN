306282273ae2d536a75ced325a770021763c7d40
MuradMT <mammadzade03@gmail.com>
2024-10-13 23:51:31 +0100

    Checked mongoose object id is valid or not.

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,25 +1,31 @@
 //Purpose: Controllers are a more advanced concept used to organize your application by separating
 // the business logic from the routing logic.
 import Workout from "../models/workout.js";
+import mongoose from "mongoose";
 
 const createWorkout = async (req, res) => {
   const { title, reps, load } = req.body;
 
   try {
     const workout = await Workout.create({ title, reps, load });
-    res.status(200).json(workout);
+    return res.status(200).json(workout);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    return res.status(400).json({ error: error.message });
   }
 };
 
 const getWorkouts = async (req, res) => {
   const workouts=await Workout.find({}).sort({createdAt:-1});
-  res.status(200).json(workouts);
+  return res.status(200).json(workouts);
 };
 
 const getWorkout = async (req, res) => {
     const {id}=req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+       return res.status(404).json({error:'No such workout'});
+    }
+
     const workout=await Workout.findById(id);
 
     return !workout 
@@ -27,4 +33,5 @@ const getWorkout = async (req, res) => {
     : res.status(200).json(workout);
 
   };
+
 export {createWorkout,getWorkouts,getWorkout};
