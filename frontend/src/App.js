d98933e61aaf810dfff0bb10fa4c97e98e8db60b
MuradMT <mammadzade03@gmail.com>
2024-10-18 23:32:51 +0100

    Finished intro to mern tutorials.

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+//npx create-react-app name creates react application for us
+//npm i react-router-dom installs react router dom
+
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
